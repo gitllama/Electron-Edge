92e91edf0cb9b29001105a1d724adf4262009227
Gitllama <23089056+gitllama@users.noreply.github.com>
2018-05-10 18:32:13 +0900

    add map

--- examples/12_mermaid/src/components/D3.jsx
+++ examples/12_mermaid/src/components/D3.jsx
@@ -5,6 +5,8 @@ import actions from '../actions'
 import Immutable from 'immutable';
 import * as d3 from 'd3';
 
+const mapconfig = require('../../map.json');
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -32,17 +34,86 @@ class App extends React.Component {
     const node = this.node
     var svg = d3.select(node)
     svg.selectAll("svg > *").remove();
-    svg.attr("height", 100)
-        .attr("width", 100);
+
+    // svg.attr("height", 200)
+    //    .attr("width", 200);
+    //
+    // svg.append("circle")
+    //    .attr("cx",100)
+    //    .attr("cy",100)
+    //    .attr("r",100)
+    //    .attr("fill","lightgray")
+    //    .attr("stroke-width",1)
+    //    .attr("stroke","black");
+
+      this.wfCreate(svg);
+       //.attr("r",10*this.props.state.get("count"))
+
+  }
+  wfCreate(svg){
+    const offsetY =mapconfig["offsetY"]
+    const offsetX = mapconfig["offsetX"]
+    const chipSizeY = mapconfig["chipSizeY"]
+    const chipSizeX = mapconfig["chipSizeX"]
+    const countY = mapconfig["countY"]
+    const countX = mapconfig["countX"]
+    const edge = mapconfig["edge"]
+
+    svg.attr("height", countY * chipSizeY)
+       .attr("width", chipSizeX * countX);
+
     svg.append("circle")
-      .attr("cx",50)
-      .attr("cy",50)
-      .attr("r",20-this.props.state.get("count"))
-      .attr("fill","green")
-      .attr("stroke-width",3)
-      .attr("stroke","orange");
-    // console.log(prevProps.txt);
-    // console.log(prevState.count);
+       .attr("cx",100 + offsetX)
+       .attr("cy",100 + offsetY)
+       .attr("r",100)
+       .attr("fill","lightgray")
+       .attr("stroke-width",1)
+       .attr("stroke","black");
+
+       svg.append("circle")
+          .attr("cx",100 + offsetX)
+          .attr("cy",100 + offsetY)
+          .attr("r",100-edge)
+          .attr("fill","none")
+          .attr("stroke-width",1)
+          .attr("stroke","black");
+
+        [...Array(countX+1)].map((_, n) =>
+          svg.append("line")
+          .attr("x1",n*chipSizeX)
+          .attr("x2",n*chipSizeX)
+          .attr("y1",0)
+          .attr("y2",chipSizeY*(countY+1))
+          .attr("stroke-width",1)
+          .attr("stroke","black")
+        );
+        [...Array(countY+1)].map((_, n) =>
+        svg.append("line")
+        .attr("x1",0)
+        .attr("x2",chipSizeX*(countX+1))
+        .attr("y1",n*chipSizeY)
+        .attr("y2",n*chipSizeY)
+        .attr("stroke-width",1)
+        .attr("stroke","black")
+      );
+
+      mapconfig["effective"].map((i)=>{
+          svg.append("rect")
+          .attr("x",chipSizeX * i["y"])
+          .attr("y",chipSizeY * i["x"])
+          .attr("width",chipSizeX)
+          .attr("height",chipSizeY)
+          .attr("fill","white")
+          .attr("stroke-width",1)
+          .attr("stroke","black");
+          svg.append("text")
+          .attr("x", chipSizeX * i["y"] + chipSizeX/2)
+          .attr("y", chipSizeY * i["x"] + chipSizeY/2 + 6)
+          .attr("text-anchor", "middle")
+          .attr("font-family","sans-serif")
+          .attr("font-size",12)
+          .text(i["n"]);
+        });
   }
   onChangeState(e){
     //this.setState({ world: e.target.value });
